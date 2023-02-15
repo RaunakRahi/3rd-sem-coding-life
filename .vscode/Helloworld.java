758c23e69b20969f3a663d6c4ca403f11636fa70
RAUNAK RAHI <70812000+RaunakRahi@users.noreply.github.com>
2023-02-15 21:30:19 +0530

    Learning_Java

---
+++ .vscode/Helloworld.java
@@ -0,0 +1,10 @@
+class FirstClass {
+    public static void main(String[] args) {
+   // Our 1st Program
+        System.out.println("i AM RAUNAK RAHI. I AM STUDYING IN RRRSDCE");
+        System.out.println("I am not going to college today");
+        System.out.println("This is my last chance to get IIT");
+        System.out.print("Hello World With hava\n from\n Apni madam");
+    }
+ }
+ 
